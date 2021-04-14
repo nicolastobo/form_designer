f6977740f3962ebffcbf7a7fa6e9d820bb400bce
Nicolas Tobo <nicolas.tobo@gmail.com>
2021-04-14 17:45:26 +0200

    config not editable

--- form_designer/models.py
+++ form_designer/models.py
@@ -86,8 +86,8 @@ class Form(models.Model):
 
     title = models.CharField(_("title"), max_length=100)
 
-    config_json = models.TextField(_("config"), blank=True)
-    config = JSONFieldDescriptor("config_json")
+    config_json = models.TextField(_("config"), blank=True, editable=False)
+    config = JSONFieldDescriptor("config_json"), editable=False)
 
     class Meta:
         verbose_name = _("form")
